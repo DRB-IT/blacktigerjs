76cbc9fb80364aa495789cfd4cd6f8f98839047f
Michael Krog <mic@apaq.dk>
2015-01-08 15:19:28 +0100

    Fixed #5

--- src/scripts/services/login.js
+++ src/scripts/services/login.js
@@ -33,7 +33,7 @@ $btmod.factory('LoginSvc', function ($q, localStorageService, $http, $rootScope,
             if (!username && !password) {
                 token = localStorageService.get('LoginToken');
             } else if (username && password) {
-                token = btoa(username + ':' + password);
+                token = btoa(username.trim() + ':' + password.trim());
             }
 
             if (token) {
