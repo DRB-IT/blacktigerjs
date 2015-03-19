5e110f60042125cfbda68299e2198e09c418177a
Michael Krog <mic@apaq.dk>
2015-03-19 15:04:05 +0100

    Prepared release

--- dist/blacktiger.js
+++ dist/blacktiger.js
@@ -1128,6 +1128,37 @@ $btmod.factory('StompSvc', ["$rootScope", function ($rootScope) {
 
 /**
  * @memberOf! blacktiger#
+ * @name SummarySvc
+ * @description
+ * 
+ * Service for retreiving summary of servers statistics
+ * 
+ * This service exposes one method: 'getSummary'.
+ * getSummary returns a promise that, when it is susccessfull, will hold an object with summary of the statistics.
+ * It will be retreived by requesting <serviceurl>/system/summary.
+ */
+$btmod.factory('SummarySvc', ["$http", "blacktiger", "$q", function ($http, blacktiger, $q) {
+    return {
+        getSummary: function () {
+            var deferred = $q.defer();
+            $http.get(blacktiger.getServiceUrl() + 'system/summary').then(function (resp) {
+                if(resp.status === 200) {
+                    deferred.resolve(resp.data);
+                } else {
+                    deferred.reject(resp);
+                }
+            });
+            return deferred.promise;
+        }
+    };
+}]);
+
+
+/*global $btmod*/
+'use strict';
+
+/**
+ * @memberOf! blacktiger#
  * @name SystemSvc
  * @description
  * 
